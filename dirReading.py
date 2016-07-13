33a803848b894774bccbdf8a7b60505f84ec8299
Kostargy <Kostargy@users.noreply.github.com>
2016-07-13 19:12:11 +0300

    delete snippet

--- dirReading.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-walk_dir = "/home/kostargy//Desktop/python/imageTest"
-
-
-for root, subdirs, files in os.walk(walk_dir):
-    for infile in files:
-        nice, ext=os.path.splitext(infile)
-        print("file:",nice," ext: ",ext)
